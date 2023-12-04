b549dc3c7e148acf89ccfa87bb47053e95ffa8ee
Noah Stride <noah.stride@goteleport.com>
2023-12-04 12:56:29 +0000

    Add protos and basic grpc server

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/strideynet/service-service
+
+go 1.21.4
+
+require (
+	google.golang.org/grpc v1.59.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/spiffe/go-spiffe/v2 v2.1.6 // indirect
+	github.com/zeebo/errs v1.3.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+)
---
+++ svc/book/cmd/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/spiffe/go-spiffe/v2/spiffegrpc/grpccredentials"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+	"github.com/strideynet/service-service/proto/book/v1"
+	"google.golang.org/grpc"
+	"log/slog"
+	"net"
+	"os"
+)
+
+func main() {
+	err := tryMain()
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func tryMain() error {
+	ctx := context.Background()
+	log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	source, err := workloadapi.NewX509Source(ctx)
+	if err != nil {
+		return fmt.Errorf("creating workloadapi src: %w", err)
+	}
+
+	bookSvc := &BookService{
+		log: log,
+	}
+	srv := grpc.NewServer(
+		grpc.Creds(
+			grpccredentials.MTLSServerCredentials(source, source, tlsconfig.AuthorizeAny()),
+		),
+	)
+	bookv1.RegisterBookServiceServer(srv, bookSvc)
+
+	lis, err := net.Listen("tcp", ":1338")
+	if err != nil {
+		return fmt.Errorf("creating listener: %w", err)
+	}
+
+	return srv.Serve(lis)
+}
+
+type BookService struct {
+	log *slog.Logger
+	bookv1.UnimplementedBookServiceServer
+}
+
+func (b *BookService) ListBooks(ctx context.Context, req *bookv1.ListBooksRequest) (*bookv1.ListBooksResponse, error) {
+	id, _ := grpccredentials.PeerIDFromContext(ctx)
+	b.log.Info("ListBooks", slog.String("id", id.String()))
+
+	return &bookv1.ListBooksResponse{
+		Books: []*bookv1.Book{
+			{
+				Isbn:  "9781098131890",
+				Title: "Identity-Native Infrastructure Access Management",
+			},
+		},
+	}, nil
+}
+
+func (b *BookService) DeleteBook(ctx context.Context, req *bookv1.DeleteBookRequest) (*bookv1.DeleteBookResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
